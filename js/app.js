4f4c5cdbe57fb4025c681575c1c21f8ce28f1697
clickys <koc.clicky@gmail.com>
2017-04-27 15:02:45 +0300

    Add on enter to post the note

--- js/app.js
+++ js/app.js
@@ -294,7 +294,7 @@
                  
              }
 
-
+             console.log(e.target);
          });
          
 
--- js/app2.js
+++ js/app2.js
@@ -37,8 +37,62 @@
         });
         
     })();
-    
-    
-    
+    var notesPositionToAdd = document.getElementById('notesPosition');
+    var newNote = document.getElementById('newNoteForm');
+    var notesTextareaBtn = document.getElementById('btnAddNotes');
+    var noteAddedDate = new Date();
+    var day = noteAddedDate.getDay();
+    var dayOfMonth = noteAddedDate.getDate();
+    var month = noteAddedDate.getMonth();
+    var hours = noteAddedDate.getHours();
+    var minutes = noteAddedDate.getMinutes();
+    var year = noteAddedDate.getFullYear().toString().substr(-2);
+    var daysArray = ['Sun,', 'Mon,', 'Thues,', 'Wed,', 'Thurs,', 'Fri,', 'Sat,'];
+    var monthsArray = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+
+    hours = (hours === 0 ? '12' : hours);
+    minutes = (minutes < 10 ? '0' : '') + minutes;
+
+    newNote.addEventListener('keydown', function (e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            var notesTextarea = document.getElementById('addNotesTextarea').value;
+            var newEl = document.createElement('div');
+            var dateEl = document.createElement('span');
+            var userTxt = document.createTextNode(notesTextarea);
+            var dateTxt = dayOfMonth + ' ' + monthsArray[month] + ' ' + year + ' ' + hours + ':' + minutes;
+            var dateNode = document.createTextNode(dateTxt);
+            var closeIcon = document.createElement('i');
+            var favourIcon = document.createElement('i');
+            var checkIcon = document.createElement('i');
+            var zoomIcon = document.createElement('i');
+            var paragraphOfUserText = document.createElement('span');
+            paragraphOfUserText.setAttribute('class', 'userTextAtNote');
+        
+            
+            closeIcon.className += " fa fa-window-close";
+            favourIcon.className = " fa fa-heart";
+            checkIcon.setAttribute('class', 'fa fa-check');
+            zoomIcon.setAttribute('class', 'fa fa-exclamation-circle');
+            paragraphOfUserText.appendChild(userTxt);
+            
+            
+
+            dateEl.appendChild(dateNode);
+            dateEl.className += " noteDateAdded";
+            
+            
+            newEl.appendChild(zoomIcon);
+            newEl.appendChild(favourIcon);
+            newEl.appendChild(checkIcon);
+            newEl.appendChild(closeIcon);
+            newEl.appendChild(paragraphOfUserText);
+            newEl.appendChild(dateEl);
+     
+            newEl.className += " col-xl-2 col-lg-2 col-md-3 col-sm-4 col-12 notesDecoration";
+            notesPositionToAdd.appendChild(newEl);
+            document.getElementById("addNotesTextarea").value = "";
+        };
+    });
     
 })();
